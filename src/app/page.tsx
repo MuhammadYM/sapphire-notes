33473dfe2a22aff964bf5e1832a63ebad66f1ada
Muhammad <muhammadalnabhani@gmail.com>
2024-02-24 23:37:17 +0000

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import { useSession } from "next-auth/react";
 import Header from "./_components/Header";
-import { api } from "~/trpc/server";
-import { getServerSession } from "next-auth/next";
-import { title } from "process";
+
 import Content from "./_components/Content";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col  text-white" data-theme="retro">
+    <main className="flex min-h-screen flex-col" data-theme="retro">
       <Header />
       <Content />
     </main>
