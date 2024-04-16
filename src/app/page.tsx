1b2ba2587c4c2cfe567c43b58d589599dca1dcc8
Muhammad <muhammadalnabhani@gmail.com>
2024-04-16 11:26:12 +0100

    created notification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,38 @@ import { useEffect } from "react";
 import Content from "./_components/Content";
 
 export default function Home() {
+  const words = [
+    "Hello",
+    "World",
+    "React",
+    "Elixir",
+    "Phoenix",
+    "JavaScript",
+    "Programming",
+  ];
   async function requestNotificationPermission() {
     try {
       const result = await Notification.requestPermission();
       if (result === "granted") {
         // Trigger your notification here
+        randomNotification();
       }
     } catch (error) {
       console.error("Error showing notification", error);
     }
+  }
+
+  function randomNotification() {
+    const randomItemIndex = Math.floor(Math.random() * words.length);
+    const notifTitle = words[randomItemIndex] ?? "Word of the Moment";
+    const notifBody = `Word of the Moment: ${words[randomItemIndex]}`;
+
+    const options = {
+      body: notifBody,
+    };
+
+    new Notification(notifTitle, options);
+    setTimeout(randomNotification, 10000);
   }
 
   useEffect(() => {
@@ -26,7 +49,9 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col" data-theme="lemonade">
-      <button id="notifications">Allow notifications!</button>
+      <button id="notifications" className="btn btn-neutral">
+        Allow notifications!
+      </button>
       <Header />
       <Content />
     </main>
