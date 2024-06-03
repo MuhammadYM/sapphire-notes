860fdaed16cb0db6488b94d8e329fa986f25e007
Muhammad <muhammadalnabhani@gmail.com>
2024-06-04 00:13:31 +0100

    wip

--- public/sw.ts
+++ public/sw.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 // Define the shape of the notification payload
 interface NotificationPayload {
   title: string;
@@ -9,11 +10,12 @@ interface NotificationPayload {
 }
 
 // Listen for push events
-self.addEventListener("push", (event: PushEvent) => {
+self.addEventListener("push", (event: Event) => {
+  const pushEvent = event as PushEvent;
   let data: NotificationPayload;
 
   try {
-    data = event.data.json();
+    data = pushEvent.data?.json();
   } catch (e) {
     console.error("Error parsing push event data:", e);
     return;
@@ -27,26 +29,29 @@ self.addEventListener("push", (event: PushEvent) => {
     },
   };
 
-  event.waitUntil(self.registration.showNotification(data.title, options));
+  Pushevent.waitUntil(self.registration.showNotification(data.title, options));
 });
 
 // Handle notification clicks
-self.addEventListener("notificationclick", (event: NotificationEvent) => {
-  event.notification.close();
-  event.waitUntil(
-    clients
-      .matchAll({ type: "window", includeUncontrolled: true })
-      .then((clientList) => {
-        if (clientList.length > 0) {
-          let client = clientList[0];
-          for (let i = 0; i < clientList.length; i++) {
-            if (clientList[i].focused) {
-              client = clientList[i];
+self.addEventListener(
+  "notificationclick",
+  (event: NotificationEvent<Notification>) => {
+    event.notification.close();
+    event.waitUntil(
+      self.Clients.matchAll({ type: "window", includeUncontrolled: true }).then(
+        (clientList: WindowClient[]) => {
+          if (clientList.length > 0) {
+            let client = clientList[0];
+            for (let i = 0; i < clientList.length; i++) {
+              if (clientList[i]?.focused) {
+                client = clientList[i];
+              }
             }
+            return client?.focus();
           }
-          return client.focus();
-        }
-        return clients.openWindow(event.notification.data.url);
-      }),
-  );
-});
+          return self.Clients.openWindow(event.notification.data.url);
+        },
+      ),
+    );
+  },
+);
